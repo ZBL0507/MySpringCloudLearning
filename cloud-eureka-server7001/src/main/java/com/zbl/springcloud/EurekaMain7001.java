26ea93854a8173889a167923204249bf1897fdcc
zbl_macO <1714217989@qq.com>
2022-04-21 20:40:09 +0800

    引入注册中心eureka

--- cloud-eureka-server7001/src/main/java/com/zbl/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/zbl/springcloud/EurekaMain7001.java
@@ -2,19 +2,17 @@ package com.zbl.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author zbl
  * @version 1.0
- * @since 2022/4/21 18:57
+ * @since 2022/4/21 20:17
  */
-@EnableEurekaClient
+@EnableEurekaServer
 @SpringBootApplication
-public class OrderMain {
+public class EurekaMain7001 {
     public static void main(String[] args) {
-
-        SpringApplication.run(OrderMain.class, args);
-
+        SpringApplication.run(EurekaMain7001.class, args);
     }
 }
--- cloud-provider-payment8001/src/main/java/com/zbl/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/zbl/springcloud/PaymentMain8001.java
@@ -3,12 +3,14 @@ package com.zbl.springcloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author zbl
  * @version 1.0
  * @since 2022/4/21 14:09
  */
+@EnableEurekaClient
 @MapperScan("com.zbl.springcloud.dao")
 @SpringBootApplication
 public class PaymentMain8001 {
