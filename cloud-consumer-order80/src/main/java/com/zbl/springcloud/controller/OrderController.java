b90fb3402dd6a439f9baf410faf7c5edf245f8d9
zbl_macO <1714217989@qq.com>
2022-04-21 22:16:31 +0800

    服务发现

--- cloud-consumer-order80/src/main/java/com/zbl/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/zbl/springcloud/controller/OrderController.java
@@ -3,8 +3,12 @@ package com.zbl.springcloud.controller;
 import com.zbl.springcloud.dto.CommonResult;
 import com.zbl.springcloud.dto.Payment;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * @author zbl
@@ -17,6 +21,9 @@ public class OrderController {
 
 //    private final static String PaymentUrl = "http://localhost:8001";
     private final static String PaymentUrl = "http://CLOUD-PAYMENT-SERVICE";
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @SuppressWarnings("all")
     @Autowired
@@ -32,5 +39,24 @@ public class OrderController {
     @GetMapping("/order/serial/{serial}")
     public CommonResult<Payment> getPayment(@PathVariable("serial") String serial) {
         return restTemplate.getForObject(PaymentUrl + "/api/payment/serial?serial=" + serial, CommonResult.class);
+    }
+
+    @GetMapping("/discovery")
+    public CommonResult<Object> getServiceInfo() {
+        List<String> services = discoveryClient.getServices();
+        services.forEach(System.out::println);
+        System.out.println("-----------------");
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        instances.forEach(e -> {
+            System.out.println("===============");
+            System.out.println(e.getMetadata());
+            System.out.println(e.getHost());
+            System.out.println(e.getServiceId());
+            System.out.println(e.getInstanceId());
+            System.out.println(e.getPort());
+            System.out.println(e.getUri());
+        });
+
+        return CommonResult.success(discoveryClient);
     }
 }
