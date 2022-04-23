cf491203416c4b298ed0aaa166b4c57f61de3614
zbl_macO <1714217989@qq.com>
2022-04-23 22:42:43 +0800

    引入网关gateway

---
+++ cloud-gateway-gateway9527/src/main/java/com/zbl/springcloud/GatewayMain9527.java
@@ -0,0 +1,18 @@
+package com.zbl.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author zbl
+ * @version 1.0
+ * @since 2022/4/23 22:24
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain9527.class, args);
+    }
+}
