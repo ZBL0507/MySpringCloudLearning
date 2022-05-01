673676e282bf835679c3fa4fa4ab6c9de287d52d
zbl_macO <1714217989@qq.com>
2022-05-01 11:03:56 +0800

    nacos注册中心

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/zbl/springcloud/NacosConfigClientMain3377.java
@@ -0,0 +1,18 @@
+package com.zbl.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author zbl
+ * @version 1.0
+ * @since 2022/5/1 10:37
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
