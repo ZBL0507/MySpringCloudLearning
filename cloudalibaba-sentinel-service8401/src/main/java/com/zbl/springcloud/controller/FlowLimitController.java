6d92e16d27c2dd951400281badb56aac4ceaae9a
zbl_macO <1714217989@qq.com>
2022-05-02 15:51:01 +0800

    sentinel热点key限流降级

--- cloudalibaba-sentinel-service8401/src/main/java/com/zbl/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/zbl/springcloud/controller/FlowLimitController.java
@@ -1,5 +1,7 @@
 package com.zbl.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +25,15 @@ public class FlowLimitController {
     public String testB() {
         log.info("test bb current thread -> " + Thread.currentThread().getName());
         return "-----test b ------";
+    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey", blockHandler = "dealTestHotKey")
+    public String testHotKey(String p1, String p2) {
+        return "---- testHotKey ----";
+    }
+
+    public String dealTestHotKey(String p1, String p2, BlockException exception) {
+        return "---- dealTestHotKey -----";
     }
 }
